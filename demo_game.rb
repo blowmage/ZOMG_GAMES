938cf97ced3ffadc96ec88a739333da3110fe3ca
Mike Moore <mike@blowmage.com>
2010-11-12 16:14:30 -0600

    Draw a square

--- demo_game.rb
+++ demo_game.rb
@@ -10,6 +10,24 @@ class DemoGame < Gosu::Window
   def button_down id
     close if id == Gosu::KbEscape
   end
+  
+  def update
+  end
+  
+  def draw
+    # Let's draw a square!
+    w, h = 200, 200
+    x = self.width/2 - w/2
+    y = self.height/2 - h/2
+    draw_rect x, y, x+w, y+h, Gosu::Color::WHITE
+  end
+  
+  def draw_rect x1, y1, x2, y2, c
+    draw_quad x1, y1, c,
+              x2, y1, c,
+              x1, y2, c,
+              x2, y2, c
+  end
 end
 
 DemoGame.new.show
