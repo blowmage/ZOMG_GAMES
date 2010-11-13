d3284eb339010df288182fe85a22675ec9344f50
Mike Moore <mike@blowmage.com>
2010-11-12 19:32:27 -0600

    Add multiple patrol creeps

--- patrol_game.rb
+++ patrol_game.rb
@@ -7,8 +7,25 @@ class PatrolGame < Gosu::Window
   def initialize width=800, height=600, fullscreen=false
     super
     
-    @creep = PatrolCreep.new self
-    @creep.color = Gosu::Color::RED
+    @creeps = (0..5).map do
+      creep = PatrolCreep.new self
+      creep.color = random_color
+      creep.start = *random_position
+      creep.end   = *random_position
+      creep
+    end
+  end
+
+  def random_color
+    color       = Gosu::Color.new 0xff000000
+    color.red   = rand(255 - 40) + 40
+    color.green = rand(255 - 40) + 40
+    color.blue  = rand(255 - 40) + 40
+    color
+  end
+  
+  def random_position
+    [ rand(self.width), rand(self.height) ]
   end
   
   def button_down id
@@ -16,11 +33,11 @@ class PatrolGame < Gosu::Window
   end
   
   def update
-    @creep.update
+    @creeps.each { |creep| creep.update }
   end
   
   def draw
-    @creep.draw
+    @creeps.each { |creep| creep.draw }
   end
 end
 
