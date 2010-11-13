79f817db75faf5fc29929636ce63ae32b336e3ef
Mike Moore <mike@blowmage.com>
2010-11-12 19:26:51 -0600

    Add start and end targets

--- patrol_creep.rb
+++ patrol_creep.rb
@@ -10,9 +10,39 @@ class PatrolCreep
     # x and y are the center of the creep
     # default to the center of the window
     @x, @y = @window.width/2, @window.height/2
+    @start = [ @circle.width, @circle.height ]
+    @end   = [ @window.width - @circle.width,
+               @window.height - @circle.height ]
+    @target = :end
   end
     
+  def target
+    @target == :start ? @start : @end
+  end
+  
+  def reverse!
+    @target = (@target == :start) ? :end : :start
+  end
+  
+  def start x, y
+    @x, @y = x, y
+    @start = [x, y]
+  end
+  
+  def end x, y
+    @end = [x, y]
+  end
+  
+  def near_target?
+    d = Gosu.distance @x, @y, target.first, target.last
+    d < @movement
+  end
+  
   def update
+    a = Gosu.angle @x, @y, target.first, target.last
+    @x += Gosu.offset_x a, @movement
+    @y += Gosu.offset_y a, @movement
+    reverse! if near_target?
   end
   
   def draw
